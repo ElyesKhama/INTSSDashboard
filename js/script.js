ada48335c7b1d482b5b06c5b4c205d4fed9e5a13
ElyesKhama <khamassi@etud.insa-toulouse.fr>
2018-08-31 11:16:30 +0200

    add sort function and maxItems to display

--- js/script.js
+++ js/script.js
@@ -4,37 +4,47 @@ var json = {
     "report": [{
         "team": "tss",
         "incidents": [
-            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T17:02:00" },
-            { "summary": "INC3", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T17:00:00" }
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+            { "summary": "INC1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T13:02:00" },
+
+            { "summary": "INC18", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T12:02:00" }
+
         ],
         "faults": [
-            { "summary": "FAU1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T19:00:00" },
-            { "summary": "FAU2", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T18:00:00" },
-            { "summary": "FAU3", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T19:00:00" }
+            { "summary": "FAU1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T12:01:00" },
+            { "summary": "FAU2", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T12:00:59" },
+            { "summary": "FAU3", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T14:00:00" }
         ],
         "changes": [
-            { "summary": "CHA1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T20:00:00" },
-            { "summary": "CHA3", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T22:00:00" }
+            { "summary": "CHA1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T15:00:00" },
+            { "summary": "CHA3", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T16:00:00" }
         ],
         "alarms": [
-            { "summary": "ALA1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T17:00:00" },
-            { "summary": "ALA2", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T18:00:00" },
-            { "summary": "ALA3", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-30T19:00:00" }
+            { "summary": "ALA1", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T17:00:00" },
+            { "summary": "ALA2", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T18:00:00" },
+            { "summary": "ALA3", "assignee": "TSS", "priority": "medium", "SLA": "2018-08-31T19:00:00" }
         ]
     },
     {
         "team": "int",
         "incidents": [
-            { "summary": "INC1", "assignee": "INT", "priority": "medium", "SLA": "2018-08-30T20:00:00" }
+            { "summary": "INC1", "assignee": "INT", "priority": "medium", "SLA": "2018-08-31T20:00:00" },
+            { "summary": "INC1", "assignee": "INT", "priority": "medium", "SLA": "2018-08-31T20:00:00" }
         ],
         "faults": [
-            { "summary": "FAU1", "assignee": "INT", "priority": "medium", "SLA": "2018-08-30T21:00:00" },
-            { "summary": "FAU2", "assignee": "INT", "priority": "medium", "SLA": "2018-08-30T22:00:00" }
+            { "summary": "FAU1", "assignee": "INT", "priority": "medium", "SLA": "2018-08-31T21:00:00" },
+            { "summary": "FAU2", "assignee": "INT", "priority": "medium", "SLA": "2018-08-31T22:00:00" }
         ],
         "changes": [
-            { "summary": "CHA1", "assignee": "INT", "priority": "medium", "SLA": "2018-08-30T23:00:00" },
-            { "summary": "CHA2", "assignee": "INT", "priority": "medium", "SLA": "2018-08-30T24:00:00" },
-            { "summary": "CHA3", "assignee": "INT", "priority": "medium", "SLA": "2018-08-31T01:00:00" }
+            { "summary": "CHA1", "assignee": "INT", "priority": "medium", "SLA": "2018-08-31T23:00:00" },
+            { "summary": "CHA2", "assignee": "INT", "priority": "medium", "SLA": "2018-08-31T24:00:00" },
+            { "summary": "CHA3", "assignee": "INT", "priority": "medium", "SLA": "2018-09-01T00:00:00" }
         ],
         "alarms": [
         ]
@@ -42,35 +52,35 @@ var json = {
     {
         "team": "pps",
         "incidents": [
-            { "summary": "INC1", "assignee": "PPS", "priority": "medium", "SLA": "2018-08-31T02:00:00" }
+            { "summary": "INC1", "assignee": "PPS", "priority": "medium", "SLA": "2018-09-01T01:00:00" }
         ],
         "faults": [
         ],
         "changes": [
-            { "summary": "CHA1", "assignee": "PPS", "priority": "medium", "SLA": "2018-08-31T03:00:00" },
-            { "summary": "CHA2", "assignee": "PPS", "priority": "medium", "SLA": "2018-08-31T04:00:00" }
+            { "summary": "CHA1", "assignee": "PPS", "priority": "medium", "SLA": "2018-09-01T03:00:00" },
+            { "summary": "CHA2", "assignee": "PPS", "priority": "medium", "SLA": "2018-09-01T04:00:00" }
         ],
         "alarms": [
-            { "summary": "ALA1", "assignee": "PPS", "priority": "medium", "SLA": "2018-08-31T05:00:00" }
+            { "summary": "ALA1", "assignee": "PPS", "priority": "medium", "SLA": "2018-09-01T05:00:00" }
         ]
     },
     {
         "team": "rti",
         "incidents": [
-            { "summary": "INC1", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T06:00:00" }
+            { "summary": "INC1", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T06:00:00" }
         ],
         "faults": [
-            { "summary": "FAU1", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T07:00:00" },
-            { "summary": "FAU2", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T08:00:00" }
+            { "summary": "FAU1", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T07:00:00" },
+            { "summary": "FAU2", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T02:00:00" }
         ],
         "changes": [
-            { "summary": "CHA1", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T08:00:00" },
-            { "summary": "CHA2", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T08:00:00" },
-            { "summary": "CHA3", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T08:00:00" }
+            { "summary": "CHA1", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T08:00:00" },
+            { "summary": "CHA2", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T08:00:00" },
+            { "summary": "CHA3", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T08:00:00" }
         ],
         "alarms": [
-            { "summary": "ALA1", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T08:00:00" },
-            { "summary": "ALA2", "assignee": "RTI", "priority": "medium", "SLA": "2018-08-31T08:00:00" }
+            { "summary": "ALA1", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T08:00:00" },
+            { "summary": "ALA2", "assignee": "RTI", "priority": "medium", "SLA": "2018-09-01T08:00:00" }
         ]
     }
     ],
@@ -80,6 +90,7 @@ var json = {
 //constants
 const listTitles = ['incidents', 'faults', 'changes', 'alarms'];
 const delay = 10000;
+const nbMaxItems = 3;
 
 function updateTitle() {
     var elem;
@@ -118,40 +129,71 @@ function updateTables(index) {
                 items = item.alarms;
                 break;
         }
-        var badgeNumber = "badgeNumber"+i;
+
+        //sort the table according to the SLA
+        items.sort(function (a, b) {
+            var aTime = a.SLA;
+            var bTime = b.SLA;
+            if (aTime < bTime) {
+                return -1;
+            } else if (aTime > bTime) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        var badgeNumber = "badgeNumber" + i;
         var elemBadgeNumber = document.getElementById(badgeNumber);
         elemBadgeNumber.innerText = items.length;
 
         //remove all rows
         removeBodyRows(elemBodyTable);
-
+        first = true;
         if (items.length != 0) {
             //fill the table
             for (var j = 0; j < items.length; j++) {
-                var line = elemBodyTable.insertRow(-1);
-                var col0 = line.insertCell(0);
-                var col1 = line.insertCell(1);
-                var col2 = line.insertCell(2);
-                var col3 = line.insertCell(3);
-                col0.innerHTML += items[j].summary;
-                col1.innerHTML += items[j].assignee;
-                col2.innerHTML += items[j].priority;
+                if (j < nbMaxItems) {   //max items to display entirely
+                    var line = elemBodyTable.insertRow(-1);
+                    var col0 = line.insertCell(0);
+                    var col1 = line.insertCell(1);
+                    var col2 = line.insertCell(2);
+                    var col3 = line.insertCell(3);
+                    col0.innerHTML += items[j].summary;
+                    col1.innerHTML += items[j].assignee;
+                    col2.innerHTML += items[j].priority;
 
-                var nbSeconds = calculationSLA(items[j].SLA);
+                    var nbSeconds = calculationSLA(items[j].SLA);
 
-                createCountDown(col3, countTimer);
-                var deadline = new Date(Date.parse(new Date()) + nbSeconds * 1000);
-                initializeClock("clockdiv" + countTimer, deadline);
+                    createCountDown(col3, countTimer);
+                    var deadline = new Date(Date.parse(new Date()) + nbSeconds * 1000);
+                    initializeClock("clockdiv" + countTimer, deadline);
+                }
+                else {
+                    var line;
+                    var div;
+                    if (first == true) {    //first time you enter in this portion of code
+                        line = elemBodyTable.insertRow(-1);
+                        div = document.createElement('div');
+                        first = false;
+                    }
+                    div.innerHTML += items[j].summary + " - ";
+                    line.appendChild(div);
 
+                }
                 countTimer++;
             }
-        } else {
+        } else {        //no items
             var line = elemBodyTable.insertRow(-1);
             line.innerHTML = "No " + listTitles[index] + " currently";
             line.style.fontStyle = "italic";
         }
     }
 }
+
+
+
+
 
 function calculationSLA(endTime) {
     var dateNow = new Date();
@@ -172,21 +214,15 @@ function createCountDown(parentElement, countTimer) {
 
     var divContainerHours = document.createElement('div');
     var spanHours = createSpan("hours");
-    // var divHours = createDiv("Hours");
     divContainerHours.appendChild(spanHours);
-    //  divContainerHours.appendChild(divHours);
 
     var divContainerMinutes = document.createElement('div');
     var spanMinutes = createSpan("minutes");
-    // var divMinutes = createDiv("Minutes");
     divContainerMinutes.appendChild(spanMinutes);
-    //  divContainerMinutes.appendChild(divMinutes);
 
     var divContainerSeconds = document.createElement('div');
     var spanSeconds = createSpan("seconds");
-    // var divSeconds = createDiv("Seconds");
     divContainerSeconds.appendChild(spanSeconds);
-    //   divContainerSeconds.appendChild(divSeconds);
 
     divClockDiv.appendChild(divContainerHours);
     divClockDiv.appendChild(divContainerMinutes);
@@ -201,14 +237,8 @@ function createSpan(type) {
     return span;
 }
 
-function createDiv(type) {
-    var div = document.createElement('div');
-    div.className = "smalltext";
-    div.innerText = type;
-    return div;
-}
-
 function removeBodyRows(elem) {
+    //remove all rows of the table
     var length = elem.getElementsByTagName("tr").length;
     for (var i = 0; i < length; i++) {
         elem.deleteRow(-1);
@@ -288,7 +318,7 @@ function updateColor(time, clockDiv) {
 function updateTimeRefresh() {
     var span = document.getElementById("timeRefresh");
     var date = new Date();
-    span.innerText = date.toTimeString().substr(0, 8);
+    span.innerText = date.toTimeString().substr(0, 8);  //to only have hour:minutes:seconds
 }
 
 function updateTitleTimer() {
